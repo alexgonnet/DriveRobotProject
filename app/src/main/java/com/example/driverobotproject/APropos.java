e723feb106d4b23ba488019c4eb0d625df92ec94
Alex Gonnet <alex.gonnet@groupe-esigelec.org>
2020-04-03 11:31:33 +0200

    add 3 activities design

---
+++ app/src/main/java/com/example/driverobotproject/APropos.java
@@ -0,0 +1,14 @@
+package com.example.driverobotproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class APropos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_propos);
+    }
+}
---
+++ app/src/main/java/com/example/driverobotproject/Parametres.java
@@ -0,0 +1,14 @@
+package com.example.driverobotproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Parametres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametres);
+    }
+}
