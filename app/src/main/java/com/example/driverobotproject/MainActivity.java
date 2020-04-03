3f19badecfa79b2ef7dfe7324dc4304febe23b18
Alex Gonnet <alex.gonnet@groupe-esigelec.org>
2020-04-03 13:18:03 +0200

    link 3 activities

--- app/src/main/java/com/example/driverobotproject/MainActivity.java
+++ app/src/main/java/com/example/driverobotproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.driverobotproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void  onButtonAProposClicked(View v)
+    {
+        Log.i("Bouton", "A propos");
+        Intent intent = new Intent(
+                MainActivity.this,
+                APropos.class
+        );
+        startActivity(intent);
+    }
+
+    public void onButtonParametresClicked(View v)
+    {
+        Log.i("Bouton", "Parametres");
+        Intent intent = new Intent(
+                MainActivity.this,
+                Parametres.class
+        );
+        startActivity(intent);
+    }
+
+    public void onButtonStartClicked(View v)
+    {
+        Log.i("Mouvement", "Start");
+    }
+
+    public void onButtonUpClicked(View v)
+    {
+        Log.i("Direction", "Up");
+    }
+
+    public void onButtonDownClicked(View v)
+    {
+        Log.i("Direction", "Down");
+    }
+
+    public void onButtonLeftClicked(View v)
+    {
+        Log.i("Direction", "Left");
+    }
+
+    public void onButtonRightClicked(View v)
+    {
+        Log.i("Direction", "Right");
     }
 }
