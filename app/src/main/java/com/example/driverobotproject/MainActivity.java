a1f13be31feea069c620b13516f3f3e5b18f0ab5
bencode20 <benjamin.bourg@groupe-esigelec.org>
2020-04-05 20:34:35 +0200

    Addition of BT version 1

--- app/src/main/java/com/example/driverobotproject/MainActivity.java
+++ app/src/main/java/com/example/driverobotproject/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.driverobotproject;
 
+/**
+ *
+ * @author Alex GONNET
+ * @version 2
+ */
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,10 +17,14 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Bluetooth bluetooth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bluetooth = new Bluetooth(getApplicationContext(), this);
+        Singleton.getInstance().bluetooth = bluetooth;
     }
 
     public void  onButtonAProposClicked(View v)
